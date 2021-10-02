3031f34573b3c1f7bf4cc548f21beb5428c06cce
simonvic <simonvic.dev@gmail.com>
2021-10-02 23:16:35 +0200

    Change sUserConfigBase to JSONSerializable

--- scripts/3_Game/sUDE/util/config/server/sServerConfig.c
+++
@@ -1,6 +0,0 @@
-/*
-*	@Singleton
-*/
-class SServerConfig{
-	
-}
--- scripts/3_Game/sUDE/util/config/user/sUserConfigBase.c
+++ scripts/3_Game/sUDE/util/config/user/sUserConfigBase.c
@@ -1,43 +1,15 @@
 
-class SUserConfigBase : Managed{
+class SUserConfigBase : SJSONSerializable{
 	
-	protected static ref JsonSerializer m_serializer = new JsonSerializer;
 	
 	//protected float serialVersionUID = 69; //@todo use serialVersionUID instead of checking the fields
 	
-	string getPath(){
-		return "";
-	}
+	string getDefaultPath();
+	string serializeDefault();
 	
-	string getDefaultPath(){
-		return "";
-	}
-	
-	void deserialize(string data, out string error){}
-	
-	string serialize(bool serializeDefault = false){
-		return "";
-	}
-	
-	void load(){
-		string data = SFileHelper.cat(getPath());
-		if(data == string.Empty) return;
-		
-		string error;
-		deserialize(data, error);
-		if(error != string.Empty){
-			SLog.e(error);
-		}
-	}		
-	
-	void save(){
-		SFileHelper.touch(getPath());
-		SFileHelper.echo(serialize(), getPath());
-	}	
-		
 	void createDefault(){
 		SFileHelper.touch(getDefaultPath());
-		SFileHelper.echo(serialize(true), getDefaultPath());
+		SFileHelper.echo(serializeDefault(), getDefaultPath());
 	}
 		
 		

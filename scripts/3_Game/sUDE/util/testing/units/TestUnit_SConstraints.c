9591886eff114f65395904be25ceb1374a35cbb8
simonvic <simonvic.dev@gmail.com>
2022-02-27 16:10:27 +0100

    Port SConstraints tests to STest framework

--- scripts/3_Game/sUDE/util/testing/units/TestUnit_SConstraints.c
+++ scripts/3_Game/sUDE/util/testing/units/TestUnit_SConstraints.c
@@ -2,95 +2,153 @@ class TestUnit_SConstraints : STestUnit {
 	
 	override void init() {
 		registerTestCases({
-			"testConstraints"
+			"enable_shouldEnable",
+			"disable_shouldDisable",
+			"simple_constrained_shouldReturnConstrainedValue",
+			"disabledConstraint_constrained_shouldReturnConstrainedValue",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue2",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue3",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue4",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue5",
+			"minMaxDictionary_constrained_shouldReturnConstrainedValue6",
+			"minMaxNumeric_costrained_shouldReturnCostrainedValue",
+			"minMaxNumeric_costrained_shouldReturnCostrainedValue2",
+			"minMaxNumeric_costrained_shouldReturnCostrainedValue3",
+			"minMaxArrayNumeric_shouldBeValidValue",
+			"minMaxArrayNumeric_costrained_shouldReturnConstrainedValue",
+			"minMaxArrayNumeric_costrained_shouldReturnConstrainedValue2",
+			"minMaxArrayNumeric_costrained_shouldReturnConstrainedValue3",
+			"minMaxArrayNumeric_costrainedOnSmallerArray_shouldReturnConstrainedValue3"
 		});
 	}
 	
-	private void testConstraints() {
-		string stringConstrained = "I've been constrained";
-		auto simpleConstrainerString = new SConstraintPrimitiveSimple<string>(stringConstrained);
-		simpleConstrainerString.setEnabled(true);
-		assertEqual(stringConstrained, simpleConstrainerString.constrained("I need to be constrained"));
-		
-		bool boolConstrained = false;
-		auto simpleConstrainerBool = new SConstraintPrimitiveSimple<bool>(boolConstrained);
-		simpleConstrainerBool.setEnabled(true);
-		assertEqual(boolConstrained, simpleConstrainerBool.constrained(true));
-		
-		bool boolConstrained2 = true;
-		auto simpleConstrainerBool2 = new SConstraintSwitch(boolConstrained2);
-		simpleConstrainerBool2.setEnabled(true);
-		assertEqual(boolConstrained2, simpleConstrainerBool2.constrained(false));
-		
-		int intConstrained = 3;
-		auto simpleConstrainerInt = new SConstraintPrimitiveSimple<int>(intConstrained);
-		simpleConstrainerInt.setEnabled(true);
-		assertEqual(intConstrained, simpleConstrainerInt.constrained(69));
-				
-		float floatConstrained = 0.420;
-		auto simpleConstrainerFloat = new SConstraintPrimitiveSimple<float>(floatConstrained);
-		simpleConstrainerFloat.setEnabled(true);
-		assertEqual(floatConstrained, simpleConstrainerFloat.constrained(6.9));
-		
-		
-		auto s_minmaxConstrainer = new SConstraintPrimitiveMinMaxDictionary("F","U");
-		s_minmaxConstrainer.setEnabled(true);
-		assertEqual("F", s_minmaxConstrainer.constrained("A"));
-		assertEqual("U", s_minmaxConstrainer.constrained("Z"));
-		assertEqual("G", s_minmaxConstrainer.constrained("G"));
-		
-		auto s_minmaxConstrainer2 = new SConstraintPrimitiveMinMaxDictionary("FFFFFF","UUU");
-		s_minmaxConstrainer2.setEnabled(true);
-		assertEqual("FFFFFF", s_minmaxConstrainer2.constrained("FF"));
-		assertEqual("UUU", s_minmaxConstrainer2.constrained("UUUUU"));
-		assertEqual("G", s_minmaxConstrainer2.constrained("G"));
-		
-		auto f_minmaxConstrainer = new SConstraintPrimitiveMinMaxNumeric(0.0, 1.0);
-		f_minmaxConstrainer.setEnabled(true);
-		assertEqual(0.0, f_minmaxConstrainer.constrained(-1.0));
-		assertEqual(1.0, f_minmaxConstrainer.constrained(2.0));
-		assertEqual(0.5, f_minmaxConstrainer.constrained(0.5));
-		
-		
-		TFloatArray min = {};
-		TFloatArray max = {};
-		TFloatArray expected = {};
-		
-		min = {0,0,0,0};
-		max = {1,1,1,1};
-		auto f_minmaxArrayConstrainer = new SConstraintMinMaxArrayNumeric({0,0,0,0}, {1,1,1,1});
-		f_minmaxArrayConstrainer.setEnabled(true);
-		assertEqual(min, f_minmaxArrayConstrainer.getMin());
-		assertEqual(max, f_minmaxArrayConstrainer.getMax());
-		
-				
-		TFloatArray s_toConstrain = {};
-		s_toConstrain = {0.5, 0.5, 0.5, 0.5};
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		assertEqual(s_toConstrain, f_minmaxArrayConstrainer.constrained(s_toConstrain));
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		
-		s_toConstrain = {-1, -1, -1, -1};
-		assertEqual(false, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		assertEqual(min, f_minmaxArrayConstrainer.constrained(s_toConstrain));
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		
-		s_toConstrain = {10, 10, 10, 10};
-		assertEqual(false, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		assertEqual(max, f_minmaxArrayConstrainer.constrained(s_toConstrain));
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		
-		s_toConstrain = {-1, 10, -1, 10};
-		expected      = {0,   1,  0,  1};
-		assertEqual(false, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		assertEqual(expected, f_minmaxArrayConstrainer.constrained(s_toConstrain));
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		
-		s_toConstrain = {-1, 10};
-		expected      = {0,   1};
-		assertEqual(false, f_minmaxArrayConstrainer.isValid(s_toConstrain));
-		assertEqual(expected, f_minmaxArrayConstrainer.constrained(s_toConstrain));
-		assertEqual(true, f_minmaxArrayConstrainer.isValid(s_toConstrain));
+	void enable_shouldEnable() {
+		auto constraint = new SConstraintBase();
+		constraint.enable();
+		assertTrue(constraint.isEnabled());
+	}
+	
+	void disable_shouldDisable() {
+		auto constraint = new SConstraintBase();
+		constraint.enable();
+		constraint.disable();
+		assertFalse(constraint.isEnabled());
+	}
+	
+	void simple_constrained_shouldReturnConstrainedValue() {
+		string constrainedValue = "Constrained string";
+		auto constraint = new SConstraintPrimitiveSimple<string>(constrainedValue);
+		constraint.enable();
+		assertEqual(constrainedValue, constraint.constrained("Unconstrained string"));
+	}
+	
+	void disabledConstraint_constrained_shouldReturnConstrainedValue() {
+		string constrainedValue = "Constrained string";
+		string unconstrainedValue = "Unconstrained string";
+		auto constraint = new SConstraintPrimitiveSimple<string>(constrainedValue);
+		constraint.disable();
+		assertEqual(unconstrainedValue, constraint.constrained(unconstrainedValue));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("F","U");
+		constraint.enable();
+		assertEqual("F", constraint.constrained("A"));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue2() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("F","U");
+		constraint.enable();
+		assertEqual("U", constraint.constrained("Z"));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue3() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("F","U");
+		constraint.enable();
+		assertEqual("G", constraint.constrained("G"));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue4() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("FFFFFF","UUU");
+		constraint.enable();
+		assertEqual("FFFFFF", constraint.constrained("FF"));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue5() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("FFFFFF","UUU");
+		constraint.enable();
+		assertEqual("UUU", constraint.constrained("UUUUU"));
+	}
+	
+	void minMaxDictionary_constrained_shouldReturnConstrainedValue6() {
+		auto constraint = new SConstraintPrimitiveMinMaxDictionary("FFFFFF","UUU");
+		constraint.enable();
+		assertEqual("G", constraint.constrained("G"));
+	}
+	
+	void minMaxNumeric_costrained_shouldReturnCostrainedValue() {
+		auto constraint = new SConstraintPrimitiveMinMaxNumeric(0.0, 1.0);
+		constraint.enable();
+		assertEqual(0.0, constraint.constrained(-1.0));
+	}
+	
+	void minMaxNumeric_costrained_shouldReturnCostrainedValue2() {
+		auto constraint = new SConstraintPrimitiveMinMaxNumeric(0.0, 1.0);
+		constraint.enable();
+		assertEqual(1.0, constraint.constrained(2.0));
+	}
+	
+	void minMaxNumeric_costrained_shouldReturnCostrainedValue3() {
+		auto constraint = new SConstraintPrimitiveMinMaxNumeric(0.0, 1.0);
+		constraint.enable();
+		assertEqual(0.5, constraint.constrained(0.5));
+	}
+
+	void minMaxArrayNumeric_shouldBeValidValue() {
+		auto constraint = new SConstraintMinMaxArrayNumeric({0,0,0,0}, {1,1,1,1});
+		TFloatArray alreadyValid = {0.5, 0.5, 0.5, 0.5};
+		assertTrue(constraint.isValid(alreadyValid));
+	}
+	
+	void minMaxArrayNumeric_costrained_shouldReturnConstrainedValue() {
+		TFloatArray min        = {0, 0, 0, 0};
+		TFloatArray max        = {1, 1, 1, 1};
+		TFloatArray toCostrain = {-1, -1, -1, -1};
+		TFloatArray expected   = {0, 0, 0, 0};
+		auto constraint = new SConstraintMinMaxArrayNumeric(min, max);
+		constraint.enable();
+		assertEqual(expected, constraint.constrained(toCostrain));
+	}
+	
+	void minMaxArrayNumeric_costrained_shouldReturnConstrainedValue2() {
+		TFloatArray min        = {0, 0, 0, 0};
+		TFloatArray max        = {1, 1, 1, 1};
+		TFloatArray toCostrain = {2, 2, 2, 2};
+		TFloatArray expected   = {1, 1, 1, 1};
+		auto constraint = new SConstraintMinMaxArrayNumeric(min, max);
+		constraint.enable();
+		assertEqual(expected, constraint.constrained(toCostrain));
+	}
+	
+	void minMaxArrayNumeric_costrained_shouldReturnConstrainedValue3() {
+		TFloatArray min        = {0, 0, 0, 0};
+		TFloatArray max        = {1, 1, 1, 1};
+		TFloatArray toCostrain = {-1, 2, -1, 2};
+		TFloatArray expected   = {0, 1, 0, 1};
+		auto constraint = new SConstraintMinMaxArrayNumeric(min, max);
+		constraint.enable();
+		assertEqual(expected, constraint.constrained(toCostrain));
+	}
+	
+	void minMaxArrayNumeric_costrainedOnSmallerArray_shouldReturnConstrainedValue3() {
+		TFloatArray min        = {0, 0, 0, 0};
+		TFloatArray max        = {1, 1, 1, 1};
+		TFloatArray toCostrain = {-1, 2};
+		TFloatArray expected   = {0, 1};
+		auto constraint = new SConstraintMinMaxArrayNumeric(min, max);
+		constraint.enable();
+		assertEqual(expected, constraint.constrained(toCostrain));
 	}
 	
 }

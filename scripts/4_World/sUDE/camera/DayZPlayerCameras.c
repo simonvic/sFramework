7684e5e2510cac9dc0dbeac6f3f9ee2ec721ee09
simonvic <simonvic.dev@gmail.com>
2024-02-03 10:13:38 +0100

    fix: register DayZ 1.24 new camera transitions

--- scripts/4_World/sUDE/camera/DayZPlayerCameras.c
+++ scripts/4_World/sUDE/camera/DayZPlayerCameras.c
@@ -120,6 +120,7 @@ modded class DayZPlayerCameras {
 		RegisterTransitionTime(DAYZCAMERA_3RD_PRO_RAISED, DAYZCAMERA_3RD_CRO_RAISED,       0.7, false);
 		RegisterTransitionTime(DAYZCAMERA_3RD_PRO_RAISED, DAYZCAMERA_3RD_ERC,              0.9, false);
 		RegisterTransitionTime(DAYZCAMERA_3RD_PRO_RAISED, DAYZCAMERA_3RD_ERC_RAISED,       0.9, false);
+		RegisterTransitionTime(DAYZCAMERA_3RD_PRO_RAISED, DAYZCAMERA_3RD_ERC_RAISED_MELEE, 0.9, false);
 
 		RegisterTransitionTime(DAYZCAMERA_3RD_VEHICLE,    DAYZCAMERA_3RD_ERC,              0.7, false);
 		
